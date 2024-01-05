f7c0b936161dcd245a6eb3d0fa895cd67876aa33
Sergey Kharchenko <skharchenko87@gmail.com>
2024-01-05 19:32:39 +0300

    216. Combination Sum III

--- medium/p0/p02/p0216/p0216. Combination Sum III_test.go
+++ medium/p0/p02/p0216/p0216. Combination Sum III_test.go
@@ -20,6 +20,8 @@ func Test_combinationSum3(t *testing.T) {
 		{"Case 1", args{3, 9}, [][]int{{1, 2, 6}, {1, 3, 5}, {2, 3, 4}}},
 		{"Case 1", args{4, 1}, [][]int{}},
 		{"Case 1", args{9, 45}, [][]int{{1, 2, 3, 4, 5, 6, 7, 8, 9}}},
+		{"Case 1", args{8, 36}, [][]int{{1, 2, 3, 4, 5, 6, 7, 8}}},
+		{"Case 1", args{4, 24}, [][]int{{1, 6, 8, 9}, {2, 5, 8, 9}, {2, 6, 7, 9}, {3, 4, 8, 9}, {3, 5, 7, 9}, {3, 6, 7, 8}, {4, 5, 6, 9}, {4, 5, 7, 8}}},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
