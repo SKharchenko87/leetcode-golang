71ab1a93dad904850e7278d15e460e990764a8db
Sergey Kharchenko <skharchenko87@gmail.com>
2024-01-13 13:20:22 +0300

    450. Delete Node in a BST

--- medium/p0/p04/p0450/p0450. Delete Node in a BST.go
+++ medium/p0/p04/p0450/p0450. Delete Node in a BST.go
@@ -1,73 +1,38 @@
 package p0450
 
 import (
-	"fmt"
 	. "leetcode/stucture"
-	"math"
 )
-
-func searchBST(root *TreeNode, val int, prev *TreeNode) (*TreeNode, *TreeNode) {
-	if root.Val == val {
-		return root, prev
-	} else if root.Val < val && root.Right != nil {
-		return searchBST(root.Right, val, root)
-	} else if root.Val > val && root.Left != nil {
-		return searchBST(root.Left, val, root)
-	}
-	return nil, prev
-}
-
-func searchMinInRight(root *TreeNode, prev *TreeNode, min int) (*TreeNode, *TreeNode) {
-	if root == nil {
-		return nil, prev
-	}
-	if min > root.Val {
-		min = root.Val
-		if root.Left != nil {
-			return searchMinInRight(root.Left, root, min)
-		} else {
-			return root, prev
-		}
-	}
-	return root, prev
-}
 
 func deleteNode(root *TreeNode, key int) *TreeNode {
 	if root == nil {
-		return nil
-	}
-	if root.Val == key && root.Right == nil && root.Left == nil {
-		return nil
-	}
-	x, prev := searchBST(root, key, nil)
-	if x == nil {
 		return root
-	}
-	if x.Right == nil && x.Left == nil {
-		if prev.Left == x {
-			prev.Left = nil
-			return root
+	} else if root.Val < key {
+		root.Right = deleteNode(root.Right, key)
+	} else if root.Val > key {
+		root.Left = deleteNode(root.Left, key)
+	} else {
+		if root.Left == nil && root.Right == nil {
+			return nil
+		} else if root.Left == nil {
+			return root.Right
+		} else if root.Right == nil {
+			return root.Left
+		} else {
+			root.Val = findMin(root.Right)
+			root.Right = deleteNode(root.Right, root.Val)
 		}
-		if prev.Right == x {
-			prev.Right = nil
-			return root
-		}
-	}
-	minInt := math.MaxInt
-	minNode, prevMinNode := searchMinInRight(x.Right, x, minInt)
-
-	fmt.Println(x.Val, prevMinNode.Val, minNode.Val)
-	if x == prevMinNode {
-		x.Val = x.Right.Val
-		x.Right = x.Right.Right
-	} else if prevMinNode.Left != nil {
-		x.Val = minNode.Val
-		prevMinNode.Left = prevMinNode.Left.Right
-	} else if prevMinNode.Right != nil {
-		x.Val = minNode.Val
-		prevMinNode.Val = minNode.Val
-		prevMinNode.Left = minNode.Left
-		prevMinNode.Right = minNode.Right
 	}
 	return root
+}
+
+func findMin(node *TreeNode) int {
+	minVal := node.Val
+	for node != nil {
+		if node.Val < minVal {
+			minVal = node.Val
+		}
+		node = node.Left
+	}
+	return minVal
 }
