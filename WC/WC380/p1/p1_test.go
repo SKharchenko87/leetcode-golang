c6c3c01a182e6305f6c5dcfcb20f122fe674bd31
Sergey Kharchenko <skharchenko87@gmail.com>
2024-01-14 08:22:07 +0300

    WC380

--- WC/WC380/p1/p1_test.go
+++ WC/WC380/p1/p1_test.go
@@ -1,1 +1,24 @@
 package p1
+
+import "testing"
+
+func Test_maxFrequencyElements(t *testing.T) {
+	type args struct {
+		nums []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{"Case 1", args{[]int{1, 2, 2, 3, 1, 4}}, 4},
+		{"Case 1", args{[]int{10, 12, 11, 9, 6, 19, 11}}, 2},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := maxFrequencyElements(tt.args.nums); got != tt.want {
+				t.Errorf("maxFrequencyElements() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
--- WC/WC380/p4/p4.go
+++ WC/WC380/p4/p4.go
@@ -1,1 +1,44 @@
 package p4
+
+import (
+	"fmt"
+	"strings"
+)
+
+func getIndexes(s, a string) []int {
+	res := []int{}
+	i := strings.Index(s, a)
+	x := 0
+	for i != -1 {
+		res = append(res, x+i)
+		x = x + i + 1
+		s = s[i+1:]
+		i = strings.Index(s, a)
+	}
+	return res
+}
+
+func beautifulIndices(s string, a string, b string, k int) []int {
+	sai := getIndexes(s, a)
+	sbi := getIndexes(s, b)
+	fmt.Println(sai)
+	fmt.Println(sbi)
+	res := []int{}
+
+	for i := 0; i < len(sai); i++ {
+		for j := len(sbi) - 1; j >= 0; j-- {
+			if abs(sbi[j]-sai[i]) <= k {
+				res = append(res, sai[i])
+				break
+			}
+		}
+	}
+	return res
+}
+
+func abs(x int) int {
+	if x < 0 {
+		return -1 * x
+	}
+	return x
+}
