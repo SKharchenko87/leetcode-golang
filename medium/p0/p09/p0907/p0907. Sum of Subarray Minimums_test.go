63933d71c38befc09d1c29d1ae8669d06fb98539
Sergey Kharchenko <skharchenko87@gmail.com>
2024-01-20 14:31:39 +0300

    907. Sum of Subarray Minimums

---
+++ medium/p0/p09/p0907/p0907. Sum of Subarray Minimums_test.go
@@ -0,0 +1,24 @@
+package p0907
+
+import "testing"
+
+func Test_sumSubarrayMins(t *testing.T) {
+	type args struct {
+		arr []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{"Case 1", args{[]int{3, 1, 2, 4}}, 17},
+		{"Case 2", args{[]int{11, 81, 94, 43, 3}}, 444},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := sumSubarrayMins(tt.args.arr); got != tt.want {
+				t.Errorf("sumSubarrayMins() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
