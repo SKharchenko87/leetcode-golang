d417b059ca5e21d94d5385c096580a3705b8ef81
Sergey Kharchenko <skharchenko87@gmail.com>
2024-01-23 16:07:29 +0300

    1239. Maximum Length of a Concatenated String with Unique Characters

---
+++ medium/p1/p12/p1239/p1239. Maximum Length of a Concatenated String with Unique Characters.go
@@ -0,0 +1,54 @@
+package p1239
+
+import (
+	"math/bits"
+)
+
+func maxLength(arr []string) int {
+	l := len(arr)
+	arrBit := make([]uint32, l)
+	for i, str := range arr {
+		tmp := strToBit(str)
+		if tmp != 0 {
+			arrBit[i] = tmp
+		}
+	}
+	maxBit := 0
+	t := make(map[uint32][]uint32, l)
+	for i := 0; i < l; i++ {
+		curStr := arrBit[i]
+		t[curStr] = []uint32{curStr}
+		maxBit = max(maxBit, bits.OnesCount32(curStr))
+		for j := 0; j < i; j++ {
+			for _, x := range t[arrBit[j]] {
+				if x&curStr == 0 {
+					t[curStr] = append(t[curStr], x+curStr)
+					maxBit = max(maxBit, bits.OnesCount32(x+curStr))
+				}
+			}
+		}
+	}
+	return maxBit
+}
+
+func strToBit(str string) uint32 {
+	var res uint32
+	m := make(map[int]int)
+	for _, c := range str {
+		k := 1 << (c - 'a')
+		m[k]++
+		if m[k] == 2 {
+			return 0
+		}
+		res += uint32(k)
+	}
+	return res
+}
+
+func max(x, y int) int {
+	if x < y {
+		return y
+	} else {
+		return x
+	}
+}
