43e9e223c17f216ce9dcba5ea94efba661603b5e
Sergey Kharchenko <skharchenko87@gmail.com>
2024-01-26 09:29:52 +0300

    1143. Longest Common Subsequence

--- medium/p1/p11/p1143/p1143. Longest Common Subsequence.go
+++ medium/p1/p11/p1143/p1143. Longest Common Subsequence.go
@@ -1,13 +1,27 @@
 package p1143
 
-// ToDo разобраться
+/*
+	   o  x  c  p  q  r  s  v  w  f
+	0  0  0  0  0  0  0  0  0  0  0
+
+s 0  0  0  0  0  0  0  1  0  0  0
+h 0  0  0  0  0  0  0  1  1  0  0
+m 0  0  0  0  0  0  0  1  1  1  0
+t 0  0  0  0  0  0  0  1  1  1  1
+u 0  0  0  0  0  0  0  1  1  1  1
+l 0  0  0  0  0  0  0  1  1  1  1
+q 0  0  0  0  0  1  0  1  1  1  1
+r 0  0  0  0  0  1  2  2  2  2  2
+y 0  0  0  0  0  1  2  2  2  2  2
+p 0  0  0  0  1  1  2  2  2  2  2
+y 0  0  0  0  1  1  2  2  2  2  2
+*/
 func longestCommonSubsequence(text1 string, text2 string) int {
 	l1, l2 := len(text1)+1, len(text2)+1
 	dp := make([][]int, l1)
-	for i := 0; i < l1; i++ {
-		dp[i] = make([]int, l2)
-	}
+	dp[0] = make([]int, l2)
 	for i := 1; i < l1; i++ {
+		dp[i] = make([]int, l2)
 		for j := 1; j < l2; j++ {
 			if text1[i-1] == text2[j-1] {
 				dp[i][j] = dp[i-1][j-1] + 1
--- medium/p1/p11/p1143/p1143. Longest Common Subsequence_test.go
+++ medium/p1/p11/p1143/p1143. Longest Common Subsequence_test.go
@@ -15,6 +15,7 @@ func Test_longestCommonSubsequence(t *testing.T) {
 		{"Case 1", args{"abcde", "ace"}, 3},
 		{"Case 2", args{"abc", "abc"}, 3},
 		{"Case 3", args{"abc", "def"}, 0},
+		{"Case 4", args{"ezupkr", "ubmrapg"}, 2},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
