f2ffc237cd53f1b531537f6b1c06c2078c35f21a
Sergey Kharchenko <skharchenko87@gmail.com>
2024-02-03 20:59:54 +0300

    1043. Partition Array for Maximum Sum

--- medium/p1/p10/p1043/p1043. Partition Array for Maximum Sum.go
+++ medium/p1/p10/p1043/p1043. Partition Array for Maximum Sum.go
@@ -2,34 +2,38 @@ package p1043
 
 import "fmt"
 
+/*
+    1, 15,  7,  9,  2,  5, 10
+0,  1, 30, 45, 54, 63, 72, 84 - лучший варинат
+       16, 37, 46, 57, 64, 83
+           31, 48, 48, 68, 82
+*/
+
 func maxSumAfterPartitioning(arr []int, k int) int {
-	l := len(arr)
-	res := make([]int, l)
-	var findMax = func(j int) (int, int) {
-		indexMax := j
-		valueMax := arr[indexMax]
-		for i := 0; i < k-1; i++ {
-			curIndex := (l + j - k + i + 1) % l
-			curValue := arr[curIndex]
-			if valueMax < curValue {
-				indexMax = curIndex
-				valueMax = curValue
-			}
+	n := len(arr)
+	dp := make([]int, n+1)
+
+	for i := 1; i <= n; i++ {
+		maxVal := 0
+		for j := 1; j <= min(i, k); j++ {
+			maxVal = max(maxVal, arr[i-j])
+			dp[i] = max(dp[i], dp[i-j]+maxVal*j)
 		}
-		return indexMax, valueMax
 	}
-	indexMax, valueMax := findMax(0)
-	res[0] = valueMax
-	for i := 1; i < l; i++ {
-		if valueMax <= arr[i] {
-			valueMax = arr[i]
-			indexMax = i
-		}
-		if indexMax == (l+i-k)%l {
-			indexMax, valueMax = findMax(i)
-		}
-		res[i] = valueMax
+	fmt.Println(dp)
+	return dp[n]
+}
+
+func min(x, y int) int {
+	if x < y {
+		return x
 	}
-	fmt.Println(res)
-	return 0
+	return y
+}
+
+func max(x, y int) int {
+	if x > y {
+		return x
+	}
+	return y
 }
