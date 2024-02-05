2aae01f80bdb98c88c5da988735fdb6a0f9ea509
Sergey Kharchenko <skharchenko87@gmail.com>
2024-02-05 14:08:46 +0300

    387. First Unique Character in a String

---
+++ easy/p0/p03/p0987/p0387. First Unique Character in a String_test.go
@@ -0,0 +1,25 @@
+package p0987
+
+import "testing"
+
+func Test_firstUniqChar(t *testing.T) {
+	type args struct {
+		s string
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{"Example 1", args{"leetcode"}, 0},
+		{"Example 2", args{"loveleetcode"}, 2},
+		{"Example 3", args{"aabb"}, -1},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := firstUniqChar(tt.args.s); got != tt.want {
+				t.Errorf("firstUniqChar() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
