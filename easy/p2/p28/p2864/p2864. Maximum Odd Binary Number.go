8aa753530f25b1ce69483d8582235e1e17c6cfce
Sergey Kharchenko <skharchenko87@gmail.com>
2024-03-04 13:47:59 +0300

    добавлен еще один вариант решения

--- easy/p2/p28/p2864/p2864. Maximum Odd Binary Number.go
+++ easy/p2/p28/p2864/p2864. Maximum Odd Binary Number.go
@@ -1,6 +1,25 @@
 package p2864
 
+//ToDo banchmark
+
 func maximumOddBinaryNumber(s string) string {
+	l := len(s)
+	res := make([]byte, l+1)
+	index0, index1 := l-1, 0
+	for i := 0; i < l; i++ {
+		if s[i] == '1' {
+			res[index1] = '1'
+			index1++
+		} else {
+			res[index0] = '0'
+			index0--
+		}
+	}
+	res[l] = '1'
+	return string(res[1:])
+}
+
+func maximumOddBinaryNumber0(s string) string {
 	l := len(s)
 	res := make([]byte, l)
 	index0, index1 := l-2, l-1
