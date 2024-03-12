10db7bc9fe53bfa8e62ec14c61d47bd77f78f43d
Sergey Kharchenko <skharchenko87@gmail.com>
2024-03-12 10:45:31 +0300

    791. Custom Sort String

---
+++ medium/p0/p07/p0791/p0791. Custom Sort String_test.go
@@ -0,0 +1,25 @@
+package p0791
+
+import "testing"
+
+func Test_customSortString(t *testing.T) {
+	type args struct {
+		order string
+		s     string
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{"Example 1", args{"cba", "abcd"}, "cbad"},
+		{"Example 2", args{"bcafg", "abcd"}, "bcad"},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := customSortString(tt.args.order, tt.args.s); got != tt.want {
+				t.Errorf("customSortString() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
