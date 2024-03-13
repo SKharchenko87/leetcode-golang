b4f62ce1c7351005dfca1f08a8045ca0989aa77b
Sergey Kharchenko <skharchenko87@gmail.com>
2024-03-13 11:43:11 +0300

    2485. Find the Pivot Integer

---
+++ medium/p2/p24/p2485/p2485. Find the Pivot Integer_test.go
@@ -0,0 +1,25 @@
+package p2485
+
+import "testing"
+
+func Test_pivotInteger(t *testing.T) {
+	type args struct {
+		n int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{"Example 1", args{8}, 6},
+		{"Example 2", args{1}, 1},
+		{"Example 3", args{4}, -1},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := pivotInteger(tt.args.n); got != tt.want {
+				t.Errorf("pivotInteger() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
