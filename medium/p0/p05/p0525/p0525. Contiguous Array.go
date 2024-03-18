d00b2f3e75b9f31ca8937901abc739a697dd6d26
Sergey Kharchenko <skharchenko87@gmail.com>
2024-03-18 17:51:05 +0300

    525. Contiguous Array

--- medium/p0/p05/p0525/p0525. Contiguous Array.go
+++ medium/p0/p05/p0525/p0525. Contiguous Array.go
@@ -3,13 +3,13 @@ package p0525
 func findMaxLength(nums []int) int {
 	res, sum := 0, 0
 	prev := make(map[int]int)
-
 	for i, val := range nums {
 		if val == 1 {
 			sum++
 		} else {
 			sum--
 		}
+
 		if sum == 0 {
 			res = i + 1
 		} else if prevId, ok := prev[sum]; !ok {
