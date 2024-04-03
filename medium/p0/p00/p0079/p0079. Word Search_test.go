0b42974f6777110154069900065c6ec3f556f9cd
Sergey Kharchenko <skharchenko87@gmail.com>
2024-04-03 12:17:01 +0300

    79. Word Search

---
+++ medium/p0/p00/p0079/p0079. Word Search_test.go
@@ -0,0 +1,26 @@
+package p0079
+
+import "testing"
+
+func Test_exist(t *testing.T) {
+	type args struct {
+		board [][]byte
+		word  string
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{"Example 1", args{[][]byte{{'A', 'B', 'C', 'E'}, {'S', 'F', 'C', 'S'}, {'A', 'D', 'E', 'E'}}, "ABCCED"}, true},
+		{"Example 2", args{[][]byte{{'A', 'B', 'C', 'E'}, {'S', 'F', 'C', 'S'}, {'A', 'D', 'E', 'E'}}, "SEE"}, true},
+		{"Example 3", args{[][]byte{{'A', 'B', 'C', 'E'}, {'S', 'F', 'C', 'S'}, {'A', 'D', 'E', 'E'}}, "ABCB"}, false},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := exist(tt.args.board, tt.args.word); got != tt.want {
+				t.Errorf("exist() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
