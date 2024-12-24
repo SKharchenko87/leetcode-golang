2a3a475fa2992c120a50bb024d2ed97e36cac90f
Sergey Kharchenko <skharchenko87@gmail.com>
2024-12-24 09:53:04 +0300

    Minimum Number of Operations to Sort a Binary Tree by Level

---
+++ medium/p2/p24/p2471/p2471. Minimum Number of Operations to Sort a Binary Tree by Level.go
@@ -0,0 +1,59 @@
+package p2471
+
+import (
+	. "leetcode/stucture"
+	"sort"
+)
+
+func minimumOperations(root *TreeNode) int {
+	result := 0
+	q := []*TreeNode{root}
+	for len(q) > 0 {
+		var lvlNums []int
+		for _, node := range q {
+			lvlNums = append(lvlNums, node.Val)
+		}
+
+		sortedNums := make([]int, len(lvlNums))
+		copy(sortedNums, lvlNums)
+
+		sort.Ints(sortedNums)
+		for idx, val := range lvlNums {
+			lvlNums[idx] = sort.SearchInts(sortedNums, val)
+		}
+
+		visited := make([]bool, len(lvlNums))
+		for _, num := range lvlNums {
+			if visited[num] {
+				continue
+			}
+
+			tmp := -1
+			for false == visited[num] {
+				visited[num] = true
+				tmp++
+				num = lvlNums[num]
+			}
+
+			result += tmp
+		}
+
+		tmp := q
+		q = nil
+		for _, node := range tmp {
+			if nil != node.Left {
+				q = append(q, node.Left)
+			}
+
+			if nil != node.Right {
+				q = append(q, node.Right)
+			}
+		}
+	}
+
+	return result
+}
+
+func run(root []int) int {
+	return minimumOperations(SliceToTreeNodeFullMinInt(root))
+}
