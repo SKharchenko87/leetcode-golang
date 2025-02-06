e9ddd7604d5f6c13ab8e6360721ab22cb403cca5
Sergey Kharchenko <skharchenko87@gmail.com>
2025-02-06 11:17:05 +0300

    Tuple with Same Product

---
+++ medium/p1/p17/p1726/p1726. Tuple with Same Product_test.go
@@ -0,0 +1,24 @@
+package p1726
+
+import "testing"
+
+func Test_tupleSameProduct(t *testing.T) {
+	type args struct {
+		nums []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{"Example 1", args{nums: []int{2, 3, 4, 6}}, 8},
+		{"Example 2", args{nums: []int{1, 2, 4, 5, 10}}, 16},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := tupleSameProduct(tt.args.nums); got != tt.want {
+				t.Errorf("tupleSameProduct() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
