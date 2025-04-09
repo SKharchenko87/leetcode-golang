43e7a2b700d6f444d512d3669d02e580070abb57
Sergey Kharchenko <skharchenko87@gmail.com>
2025-04-09 14:20:38 +0300

    Add benchmark test

--- easy/p0/p00/p0070/p0070_Climbing_Stairs.go
+++ easy/p0/p00/p0070/p0070_Climbing_Stairs.go
@@ -1,6 +1,14 @@
 package p0070
 
 func climbStairs(n int) int {
+	cur, prev := 1, 1
+	for i := 1; i < n; i++ {
+		cur, prev = cur+prev, cur
+	}
+	return cur
+}
+
+func climbStairs0(n int) int {
 	switch n {
 	case 0, 1, 2, 3:
 		return n
@@ -10,4 +18,14 @@ func climbStairs(n int) int {
 		cur, prev = cur+prev, cur
 	}
 	return cur
+}
+
+func climbStairs1(n int) int {
+	dp := [46]int{}
+	dp[1] = 1
+	dp[2] = 2
+	for i := 3; i <= n; i++ {
+		dp[i] = dp[i-1] + dp[i-2]
+	}
+	return dp[n]
 }
