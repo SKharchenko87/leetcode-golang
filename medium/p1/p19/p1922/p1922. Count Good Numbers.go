10adacd4d3367e64860d9eaa364ea5f21daaae72
Sergey Kharchenko <skharchenko87@gmail.com>
2025-04-13 19:38:30 +0300

    powerWithMOD

--- medium/p1/p19/p1922/p1922. Count Good Numbers.go
+++ medium/p1/p19/p1922/p1922. Count Good Numbers.go
@@ -3,16 +3,16 @@ package p1922
 const MOD = 1e9 + 7
 
 func countGoodNumbers(n int64) int {
-	return int(powQuick(5, (n+1)/2) * powQuick(4, n/2) % MOD)
+	return int(powerWithMOD64(5, (n+1)/2, MOD) * powerWithMOD64(4, n/2, MOD) % MOD)
 }
 
-func powQuick(x, y int64) int64 {
+func powerWithMOD64(x, y, mod int64) int64 {
 	res := int64(1)
 	for y != 0 {
 		if y%2 == 1 {
-			res = res * x % MOD
+			res = res * x % mod
 		}
-		x = x * x % MOD
+		x = x * x % mod
 		y /= 2
 	}
 	return res
