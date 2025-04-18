7366f3380bf61c0dde3eb39be043b69b48bbbe93
Sergey Kharchenko <skharchenko87@gmail.com>
2025-04-18 13:25:39 +0300

    Count and Say

---
+++ medium/p0/p00/p0038/p0038. Count and Say_test.go
@@ -0,0 +1,24 @@
+package main
+
+import "testing"
+
+func Test_countAndSay(t *testing.T) {
+	type args struct {
+		n int
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{"Example 1", args{n: 4}, "1211"},
+		{"Example 2", args{n: 1}, "1"},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := countAndSay(tt.args.n); got != tt.want {
+				t.Errorf("countAndSay() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
