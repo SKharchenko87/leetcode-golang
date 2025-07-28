7f380f200ea44ea0e69a0893407e6856efd81468
Sergey Kharchenko <skharchenko87@gmail.com>
2025-07-28 12:35:58 +0300

    Count Number of Maximum Bitwise-OR Subsets

--- medium/p2/p20/p2044/p2044. Count Number of Maximum Bitwise-OR Subsets.go
+++ medium/p2/p20/p2044/p2044. Count Number of Maximum Bitwise-OR Subsets.go
@@ -1,6 +1,88 @@
 package p2044
 
+import (
+	"sort"
+)
+
+// ToDo еще есть варианты через Memorization и Bit Manipulation + Dynamic Programming
+
 func countMaxOrSubsets(nums []int) int {
+	maxOR := 0
+	l := len(nums)
+	for i := 0; i < l; i++ {
+		maxOR |= nums[i]
+	}
+
+	var backTrack func(index int, current int) int
+	backTrack = func(index int, current int) int {
+		// Если достигли максимального значения, то все остальные варианты будут равны maxOR
+		if current == maxOR {
+			return 1 << (l - 1 - index)
+		}
+		index++
+		if index == l {
+			return 0
+		}
+		// С учетом значения из nums[nextIndex] + без учета nums[nextIndex]
+		return backTrack(index, current|nums[index]) + backTrack(index, current)
+	}
+
+	return backTrack(0, nums[0]) + backTrack(0, 0)
+}
+
+func countMaxOrSubsets3(nums []int) int {
+	maxOR := 0
+	l := len(nums)
+	for i := 0; i < l; i++ {
+		maxOR |= nums[i]
+	}
+
+	sort.Ints(nums)
+
+	var backTrack func(index int, isIncluded bool, current int) int
+	backTrack = func(index int, isIncluded bool, current int) int {
+
+		if isIncluded {
+			current |= nums[index]
+		}
+		if index == 0 {
+			if current == maxOR {
+				return 1
+			}
+			return 0
+		}
+		return backTrack(index-1, true, current) + backTrack(index-1, false, current)
+	}
+
+	return backTrack(l-1, true, 0) + backTrack(l-1, false, 0)
+}
+
+func countMaxOrSubsets2(nums []int) int {
+	maxOR := 0
+	l := len(nums)
+	for i := 0; i < l; i++ {
+		maxOR |= nums[i]
+	}
+	result := 1
+	// Полный перебор возможных вариантов (позиция бита числа указывает используется ли элемент из nums)
+	for i := 1; i < 1<<l-1; i++ {
+		currenOR := 0
+		for indexBit := 0; indexBit < l; indexBit++ {
+			// Если в варианте используется nums[indexBit], то учитываем его значение currentOR
+			if (i>>indexBit)&1 == 1 {
+				currenOR |= nums[indexBit]
+			}
+			// Как только текущее значение достигнет максимально добавляем этот вариант в результат
+			if currenOR == maxOR {
+				result++
+				break
+			}
+		}
+	}
+	return result
+}
+
+func countMaxOrSubsets1(nums []int) int {
 	l := len(nums)
 	sum := 0
 	for i := 0; i < l; i++ {
