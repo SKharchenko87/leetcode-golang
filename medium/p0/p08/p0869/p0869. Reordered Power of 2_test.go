820d5118527dd79ec88d1976cd3faf3fa1ab09b3
Sergey Kharchenko <skharchenko87@gmail.com>
2025-08-10 10:58:08 +0300

    Reordered Power of 2

---
+++ medium/p0/p08/p0869/p0869. Reordered Power of 2_test.go
@@ -0,0 +1,25 @@
+package p0869
+
+import "testing"
+
+func Test_reorderedPowerOf2(t *testing.T) {
+	type args struct {
+		n int
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{"Example 1", args{n: 1}, true},
+		{"Example 2", args{n: 10}, false},
+		{"My 4210", args{n: 4210}, true},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := reorderedPowerOf2(tt.args.n); got != tt.want {
+				t.Errorf("reorderedPowerOf2() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
