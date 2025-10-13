1cdc72126e86f6e28850c761840d15ed1a801992
Sergey Kharchenko <skharchenko87@gmail.com>
2025-10-13 11:39:51 +0300

    Taking Maximum Energy From the Mystic Dungeon

---
+++ medium/p3/p31/p3147/p3147. Taking Maximum Energy From the Mystic Dungeon_test.go
@@ -0,0 +1,25 @@
+package p3147
+
+import "testing"
+
+func Test_maximumEnergy(t *testing.T) {
+	type args struct {
+		energy []int
+		k      int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{"Example 1", args{energy: []int{5, 2, -10, -5, 1}, k: 3}, 3},
+		{"Example 2", args{energy: []int{-2, -3, -1}, k: 2}, -1},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := maximumEnergy(tt.args.energy, tt.args.k); got != tt.want {
+				t.Errorf("maximumEnergy() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
