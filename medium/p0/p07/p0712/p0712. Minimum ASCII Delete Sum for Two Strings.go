080b68e9ddf423a3c022dd34bedc4f8c44cfdcce
Sergey Kharchenko <contact@sergeykharchenko.ru>
2026-01-10 13:33:13 +0300

    Оптимизация для сравнения от перестановки строк s1 и s2 местами

--- medium/p0/p07/p0712/p0712. Minimum ASCII Delete Sum for Two Strings.go
+++ medium/p0/p07/p0712/p0712. Minimum ASCII Delete Sum for Two Strings.go
@@ -6,14 +6,6 @@ e  . * . * . *
 e  . * . * . *
 t  . . . . * .
 
-l
-le
-e
-t
-
-lt
-ee
-et
 let
 lee
 eet
@@ -21,6 +13,71 @@ eet
 package p0712
 
 func minimumDeleteSum(s1 string, s2 string) int {
+	totalSum := 0
+	l1, l2 := len(s1), len(s2)
+
+	if l1 < l2 { // < для ns/op; > для B/op
+		s1, s2 = s2, s1
+		l1, l2 = l2, l1
+	}
+
+	for i := 0; i < l1; i++ {
+		totalSum += int(s1[i])
+	}
+	for i := 0; i < l2; i++ {
+		totalSum += int(s2[i])
+	}
+
+	prevDP := make([]int, l2+1)
+	currDP := make([]int, 1, l2+1)
+
+	var cross int
+	for i := 1; i <= l1; i++ {
+		for j := 1; j <= l2; j++ {
+			if s1[i-1] == s2[j-1] {
+				cross = int(s1[i-1])
+			} else {
+				cross = 0
+			}
+			currDP = append(currDP, max(currDP[j-1], prevDP[j], prevDP[j-1]+cross))
+		}
+		prevDP, currDP = currDP, prevDP
+		currDP = currDP[:1]
+	}
+	return totalSum - 2*prevDP[l2]
+}
+
+func minimumDeleteSum1(s1 string, s2 string) int {
+	totalSum := 0
+	l1, l2 := len(s1), len(s2)
+
+	for i := 0; i < l1; i++ {
+		totalSum += int(s1[i])
+	}
+	for i := 0; i < l2; i++ {
+		totalSum += int(s2[i])
+	}
+
+	prevDP := make([]int, l2+1)
+	currDP := make([]int, 1, l2+1)
+
+	var cross int
+	for i := 1; i <= l1; i++ {
+		for j := 1; j <= l2; j++ {
+			if s1[i-1] == s2[j-1] {
+				cross = int(s1[i-1])
+			} else {
+				cross = 0
+			}
+			currDP = append(currDP, max(currDP[j-1], prevDP[j], prevDP[j-1]+cross))
+		}
+		prevDP, currDP = currDP, prevDP
+		currDP = currDP[:1]
+	}
+	return totalSum - 2*prevDP[l2]
+}
+
+func minimumDeleteSum0(s1 string, s2 string) int {
 	totalSum := 0
 	l1, l2 := len(s1), len(s2)
 	for i := 0; i < l1; i++ {
