abf2416eb4c738d96a6336737090cf3f4ac6a9d5
Sergey Kharchenko <contact@sergeykharchenko.ru>
2026-01-12 09:20:44 +0300

    Minimum Time Viditing All Points

---
+++ easy/p1/p12/p1266/p1266. Minimum Time Viditing All Points.go
@@ -0,0 +1,18 @@
+package p1266
+
+func minTimeToVisitAllPoints(points [][]int) int {
+	res := 0
+	for i := 1; i < len(points); i++ {
+		dx, dy := abs(points[i][0]-points[i-1][0]), abs(points[i][1]-points[i-1][1])
+		maxD := max(dx, dy)
+		res += maxD
+	}
+	return res
+}
+
+func abs(x int) int {
+	if x < 0 {
+		x *= -1
+	}
+	return x
+}
