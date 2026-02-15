0f2f632908a07af5c699cd867ce7450fad67f00c
Sergey Kharchenko <contact@sergeykharchenko.ru>
2026-02-15 10:15:46 +0300

    Add Binary

---
+++ easy/p0/p00/p0067/p0067. Add Binary_test.go
@@ -0,0 +1,25 @@
+package main
+
+import "testing"
+
+func Test_addBinary(t *testing.T) {
+	type args struct {
+		a string
+		b string
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{"Example 1", args{a: "11", b: "1"}, "100"},
+		{"Example 2", args{a: "1010", b: "1011"}, "10101"},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := addBinary(tt.args.a, tt.args.b); got != tt.want {
+				t.Errorf("addBinary() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
