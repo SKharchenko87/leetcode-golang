e0c1540c3e5f76a25472054f32e1dc6d409593c1
Sergey Kharchenko <contact@sergeykharchenko.ru>
2026-02-18 09:34:38 +0300

    Binary Number with Alternating Bits

---
+++ easy/p0/p06/p0693/p0693. Binary Number with Altenating Bits_test.go
@@ -0,0 +1,26 @@
+package p0693
+
+import "testing"
+
+func Test_hasAlternatingBits(t *testing.T) {
+	type args struct {
+		n int
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{"Example 1", args{n: 5}, true},
+		{"Example 2", args{n: 7}, false},
+		{"Example 3", args{n: 11}, false},
+		{"TestCase 176", args{n: 4}, false},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := hasAlternatingBits(tt.args.n); got != tt.want {
+				t.Errorf("hasAlternatingBits() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
