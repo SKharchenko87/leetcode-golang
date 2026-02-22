6b6cb15e3eaf024d5d349b9383f0d9bf054c6e00
Sergey Kharchenko <contact@sergeykharchenko.ru>
2026-02-22 08:15:17 +0300

    Prime Number of Set Bits in Binary Representation

---
+++ easy/p0/p07/p0762/p0762. Prime Number of Set Bits in Binary Representation_test.go
@@ -0,0 +1,24 @@
+package p0762
+
+import "testing"
+
+func Test_countPrimeSetBits(t *testing.T) {
+	type args struct {
+		left  int
+		right int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := countPrimeSetBits(tt.args.left, tt.args.right); got != tt.want {
+				t.Errorf("countPrimeSetBits() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
