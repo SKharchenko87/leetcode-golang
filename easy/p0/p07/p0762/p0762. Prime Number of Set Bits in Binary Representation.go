3c1223d31516419aa214e8806188886b036f4fa4
Sergey Kharchenko <contact@sergeykharchenko.ru>
2026-02-22 08:15:53 +0300

    Prime Number of Set Bits in Binary Representation

--- easy/p0/p07/p0762/p0762. Prime Number of Set Bits in Binary Representation.go
+++ easy/p0/p07/p0762/p0762. Prime Number of Set Bits in Binary Representation.go
@@ -1,1 +1,17 @@
 package p0762
+
+import "math/bits"
+
+// max 1111_0100_0010_0100_0000
+
+var primes = map[int]struct{}{2: {}, 3: {}, 5: {}, 7: {}, 11: {}, 13: {}, 17: {}, 19: {}}
+
+func countPrimeSetBits(left, right int) int {
+	cnt := 0
+	for i := uint(left); i <= uint(right); i++ {
+		if _, ok := primes[bits.OnesCount(i)]; ok {
+			cnt++
+		}
+	}
+	return cnt
+}
--- easy/p0/p07/p0762/p0762. Prime Number of Set Bits in Binary Representation_test.go
+++ easy/p0/p07/p0762/p0762. Prime Number of Set Bits in Binary Representation_test.go
@@ -12,7 +12,9 @@ func Test_countPrimeSetBits(t *testing.T) {
 		args args
 		want int
 	}{
-		// TODO: Add test cases.
+		{"Example 1", args{left: 6, right: 10}, 4},
+		{"Example 2", args{left: 10, right: 15}, 5},
+		{"TestCase 172", args{left: 248657, right: 257888}, 3381},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
