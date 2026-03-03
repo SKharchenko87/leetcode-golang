90bc966ce4c43a20f50177cce0ab5bdd27813c35
Sergey Kharchenko <contact@sergeykharchenko.ru>
2026-03-03 17:06:00 +0300

    Number of Steps to Reduce a Number in Binary Representation to One

--- medium/p1/p14/p1404/p1404. Number of Steps to Reduce a Number in Binary Representation to One.go
+++ medium/p1/p14/p1404/p1404. Number of Steps to Reduce a Number in Binary Representation to One.go
@@ -1,6 +1,48 @@
 package p1404
 
+import "strings"
+
 func numSteps(s string) int {
+	end := strings.LastIndex(s, "1") + 1
+	tailZeros := len(s) - end
+	if end == 1 {
+		return tailZeros
+	}
+	return end*2 - strings.Count(s[:end], "1") + tailZeros + 1
+}
+
+func numSteps5(s string) int {
+	trainZeros := 0
+	i := len(s) - 1
+	for ; i > 0 && s[i] == '0'; i-- {
+		trainZeros++
+	}
+	if i == 0 {
+		return trainZeros
+	}
+	i++
+	ones := strings.Count(s[:i], "1")
+	zeros := i - ones
+	return zeros*2 + trainZeros + ones + 1
+}
+
+func numSteps4(s string) int {
+	ones := strings.Count(s, "1")
+	zeros := len(s) - ones
+	tailZeros := 0
+	for i := len(s) - 1; i > 0 && s[i] == '0'; i-- {
+		tailZeros++
+	}
+	if tailZeros == zeros {
+		if ones == 1 {
+			return zeros
+		}
+		return zeros + ones + 1
+	}
+	return zeros*2 - tailZeros + ones + 1
+}
+
+func numSteps3(s string) int {
 	res := 0
 	i := len(s) - 1
 	for ; i >= 0 && s[i] != '1'; i-- {
@@ -14,6 +56,22 @@ func numSteps(s string) int {
 		res++
 		if s[i] == '0' {
 			res++
+		}
+	}
+	return res
+}
+
+func numSteps2(s string) int {
+	res := 0
+	hasOne := false
+	for i := len(s) - 1; i >= 0; i-- {
+		res++
+		if s[i] == '0' {
+			if hasOne {
+				res++
+			}
+		} else {
+			hasOne = true
 		}
 	}
 	return res
--- medium/p1/p14/p1404/p1404. Number of Steps to Reduce a Number in Binary Representation to One_test.go
+++ medium/p1/p14/p1404/p1404. Number of Steps to Reduce a Number in Binary Representation to One_test.go
@@ -1,6 +1,8 @@
 package p1404
 
-import "testing"
+import (
+	"testing"
+)
 
 func Test_numSteps(t *testing.T) {
 	type args struct {
@@ -25,4 +27,39 @@ func Test_numSteps(t *testing.T) {
 			}
 		})
 	}
+}
+
+func bench(f func(s string) int, b *testing.B) {
+	b.ReportAllocs()
+	for n := 0; n < b.N; n++ {
+		f("11111100100010100010010110100111111110000010000010110110110101000001100100110111110001111001110010101001111101101011110011100110101101111001111111101101011110110000110010010011100100100101100100101110000101010101010101010000011101000100001100111101010001010110110011111000000000101010101010011111001111110010010001010000100101101000110000100011100110110011010100101100100110001011011000101001111110110110100111100111110110100111010001000001110111111001111000110111111100010001111000110101001101010000")
+	}
+}
+
+func Benchmark_numSteps(b *testing.B) {
+	bench(numSteps, b)
+}
+
+func Benchmark_numSteps5(b *testing.B) {
+	bench(numSteps5, b)
+}
+
+func Benchmark_numSteps4(b *testing.B) {
+	bench(numSteps4, b)
+}
+
+func Benchmark_numSteps3(b *testing.B) {
+	bench(numSteps3, b)
+}
+
+func Benchmark_numSteps2(b *testing.B) {
+	bench(numSteps2, b)
+}
+
+func Benchmark_numSteps1(b *testing.B) {
+	bench(numSteps1, b)
+}
+
+func Benchmark_numSteps0(b *testing.B) {
+	bench(numSteps0, b)
 }
