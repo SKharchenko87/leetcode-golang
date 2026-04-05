545cbdc82596faf5388d92a2e6f71cef4809a541
Sergey Kharchenko <contact@sergeykharchenko.ru>
2026-04-05 11:39:42 +0300

    Robot Return to Origin

---
+++ easy/p0/p06/p0657/p0675. Robot Return to Origin_test.go
@@ -0,0 +1,24 @@
+package p0657
+
+import "testing"
+
+func Test_judgeCircle(t *testing.T) {
+	type args struct {
+		moves string
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{"Example 1", args{moves: "UD"}, true},
+		{"Example 2", args{moves: "LL"}, false},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := judgeCircle(tt.args.moves); got != tt.want {
+				t.Errorf("judgeCircle() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
