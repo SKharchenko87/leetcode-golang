26e64e85744ff4af3d009cdf896436ecbb2795be
Sergey Kharchenko <contact@sergeykharchenko.ru>
2026-04-10 12:25:38 +0300

    Minimum Distance Between Three Equal Elements I

---
+++ easy/p3/p37/p3740/p3740. Minimum Distance Between Three Equal Elements I_test.go
@@ -0,0 +1,26 @@
+package p3740
+
+import "testing"
+
+func Test_minimumDistance(t *testing.T) {
+	type args struct {
+		nums []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{"Example 1", args{nums: []int{1, 2, 1, 1, 3}}, 6},
+		{"Example 2", args{nums: []int{1, 1, 2, 3, 2, 1, 2}}, 8},
+		{"Example 3", args{nums: []int{1}}, -1},
+		{"TestCase 568", args{nums: []int{1, 2, 2}}, -1},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := minimumDistance(tt.args.nums); got != tt.want {
+				t.Errorf("minimumDistance() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
