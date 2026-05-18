b194abcceaa0a0b211dbde1807141fc3c6305105
Sergey Kharchenko <contact@sergeykharchenko.ru>
2026-05-18 10:09:52 +0300

    Find Minimum in Rotated Sorted Array II

---
+++ hard/p0/p01/p0154/p0154. Find Minimum in Rotated Sorted Array II.go
@@ -0,0 +1,7 @@
+package p0154
+
+import "slices"
+
+func findMin(nums []int) int {
+	return slices.Min(nums)
+}
---
+++ hard/p0/p01/p0154/p0154. Find Minimum in Rotated Sorted Array II_test.go
@@ -0,0 +1,24 @@
+package p0154
+
+import "testing"
+
+func Test_findMin(t *testing.T) {
+	type args struct {
+		nums []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{"Example 1", args{nums: []int{1, 3, 5}}, 1},
+		{"Example 2", args{nums: []int{2, 2, 2, 0, 1}}, 0},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := findMin(tt.args.nums); got != tt.want {
+				t.Errorf("findMin() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
