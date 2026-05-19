a01595ddb790346b8bad6b4b1f705eebc4661a01
Sergey Kharchenko <contact@sergeykharchenko.ru>
2026-05-19 11:40:50 +0300

    Minimum Common Value

--- easy/p2/p25/p2540/p2540. Minimum Common Value.go
+++ easy/p2/p25/p2540/p2540. Minimum Common Value.go
@@ -1,6 +1,35 @@
 package p2540
 
+import "slices"
+
 func getCommon(nums1 []int, nums2 []int) int {
+	if len(nums1) > len(nums2) {
+		nums1, nums2 = nums2, nums1
+	}
+	for i := 0; i < len(nums1); i++ {
+		if _, ok := slices.BinarySearch(nums2, nums1[i]); ok {
+			return nums1[i]
+		}
+	}
+	return -1
+}
+
+func getCommon1(nums1 []int, nums2 []int) int {
+	i, j := 0, 0
+	for i < len(nums1) && j < len(nums2) {
+		if nums1[i] == nums2[j] {
+			return nums1[i]
+		}
+		if nums1[i] < nums2[j] {
+			i++
+		} else {
+			j++
+		}
+	}
+	return -1
+}
+
+func getCommon0(nums1 []int, nums2 []int) int {
 	l1, l2 := len(nums1), len(nums2)
 	i1, i2 := 0, 0
 	for i1 < l1 && i2 < l2 {
