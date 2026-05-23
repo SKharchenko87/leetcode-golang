3bceb16b81a22c4e490c37c35adb9863d7d1b024
Sergey Kharchenko <contact@sergeykharchenko.ru>
2026-05-23 10:42:46 +0300

    Check if Array is Sorted and Rotated

--- easy/p1/p17/p1752/p1752. Check if Array Is Sorted and Rotated.go
+++ easy/p1/p17/p1752/p1752. Check if Array Is Sorted and Rotated.go
@@ -1,6 +1,32 @@
 package p1752
 
+import "slices"
+
 func check(nums []int) bool {
+	cnt := 0
+	n := len(nums)
+
+	for i := 0; i < n && cnt < 2; i++ {
+		if nums[i] > nums[(i+1)%n] {
+			cnt++
+		}
+	}
+
+	return cnt < 2
+}
+
+func check1(nums []int) bool {
+	cnt := 1
+	_ = slices.IsSortedFunc(nums, func(a, b int) int {
+		if a < b {
+			cnt--
+		}
+		return cnt
+	})
+	return cnt == 1 || cnt == 0 && nums[0] >= nums[len(nums)-1]
+}
+
+func check0(nums []int) bool {
 	l := len(nums)
 	cntSplit := 0
 	if nums[0] >= nums[l-1] {
--- easy/p1/p17/p1752/p1752. Check if Array Is Sorted and Rotated_test.go
+++ easy/p1/p17/p1752/p1752. Check if Array Is Sorted and Rotated_test.go
@@ -23,3 +23,22 @@ func Test_check(t *testing.T) {
 		})
 	}
 }
+
+func bench(b *testing.B, f func(nums []int) bool) {
+	b.ReportAllocs()
+	for i := 0; i < b.N; i++ {
+		f([]int{38, 8, 53, 18, 57, 44, 56, 58, 57, 93, 56, 12, 89, 75, 37, 45, 57, 40, 67, 100, 6, 92, 100, 87, 47, 67, 47, 38, 16, 21, 48, 29, 21, 61, 29, 84, 9, 23, 43, 22, 84, 50, 7, 21, 70, 31, 30, 22, 2, 76, 33, 25, 11, 51, 74, 95, 94, 2, 50, 49, 55, 43, 93, 60, 44, 97, 52, 1, 63, 97, 66, 20, 84, 4, 85, 14, 85, 18, 15, 26, 51, 70, 52, 8, 95, 84, 37, 4, 41, 97, 96, 17, 20, 98, 34, 70, 93, 83, 81, 62})
+	}
+}
+
+func Benchmark_check(b *testing.B) {
+	bench(b, check)
+}
+
+func Benchmark_check1(b *testing.B) {
+	bench(b, check1)
+}
+
+func Benchmark_check0(b *testing.B) {
+	bench(b, check0)
+}
