945d6d540d26e8b1bf395fe14ee0216105204cca
Sergey Kharchenko <contact@sergeykharchenko.ru>
2026-06-20 11:31:33 +0300

    Find the Highest Altitude

---
+++ easy/p1/p17/p1732/p1732. Find the Highest Altitude_test.go
@@ -0,0 +1,23 @@
+package p1732
+
+import "testing"
+
+func Test_largestAltitude(t *testing.T) {
+	type args struct {
+		gain []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := largestAltitude(tt.args.gain); got != tt.want {
+				t.Errorf("largestAltitude() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
